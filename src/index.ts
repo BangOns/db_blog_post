02bbb6bc6c00e5d0bd5e6b3e30c9dd9ffbbfa26e
bangOns <syahr9950@gmail.com>
2025-07-24 11:54:42 +0700

    feat: Fixing bug

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,23 @@ import route from "./route/index";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import cors from "cors";
-const express = require("express");
+import express from "express";
 
 const app = express();
+const allowedOrigins = ["http://localhost:5173"];
 app.use(
   cors({
-    origin: "*",
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.use(express.json());
 dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 const port = process.env.PORT || 3001;
 app.use(route);
 app.use("/", (req: Request, res: Response) => {
