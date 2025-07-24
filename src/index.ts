08452756ced54557aa2eff9d579d28c84f5b4fdf
bangOns <syahr9950@gmail.com>
2025-07-24 11:46:01 +0700

    fix: Fixing Bug

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import * as dotenv from "dotenv";
 import route from "./route/index";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
+import cors from "cors";
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 dotenv.config();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3001;
 app.use(route);
 app.use("/", (req: Request, res: Response) => {
