e401ae88195c852dd1ea3e15cf5477c5d821bdc3
bangOns <syahr9950@gmail.com>
2025-07-25 18:01:35 +0700

    fix: Fixing bug again

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ dotenv.config();
 import route from "./route/index";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
-import cors from "cors";
 import express from "express";
 
 const app = express();
+const cors = require("cors");
 const allowedOrigins = [
   "http://localhost:5173",
   "https://db-blog-post.vercel.app",
 ];
+
 app.use(
   cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
