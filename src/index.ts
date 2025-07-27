75b398da5290c0686e7e3ab1efc99b476818404c
bangOns <syahr9950@gmail.com>
2025-07-27 10:36:41 +0700

    fix: Fixing bug

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,34 @@ const app = express();
 
 require("dotenv").config();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://db-blog-post.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = process.env.PORT || 3001;
 app.use(route);
 app.use("/", (req: Request, res: Response) => {
   res.status(404).send("page not found");
 });
 
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
