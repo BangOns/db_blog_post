fed3f6c958175f970d808777b512b18b0be6a17d
bangOns <syahr9950@gmail.com>
2025-07-27 13:26:26 +0700

    fix: Fixing Bug

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,13 @@
 import route from "./route/index";
 import * as bodyParser from "body-parser";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.headers.origin;
-  const allowedOrigins = [
-    "http://localhost:5173",
-    "https://db-blog-post.vercel.app",
-  ];
-
-  // Always set CORS headers
-  if (allowedOrigins.includes(origin as string)) {
-    res.setHeader("Access-Control-Allow-Origin", origin as string);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  // Handle preflight
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
